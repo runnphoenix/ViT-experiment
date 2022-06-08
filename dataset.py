34ab7aa6af5cad2aaaa0c879ce6c4f781f156b5c
Zhang Hanying <runnphoenix@gmail.com>
2022-06-08 13:49:12 +0800

    re-organise code

--- dataset.py
+++ dataset.py
@@ -15,30 +15,22 @@ class DoCaSet(Dataset):
 
         self.category = category
 
-        root_path = '../cat_dog'
-        if category == 'train':
-            data_root = os.path.join(root_path, 'train')
-        elif category == 'val':
-            data_root = os.path.join(root_path, 'val')
-        elif category == 'test':
-            data_root =  os.path.join(root_path, 'test')
+        if category == 'train' or category == 'val' or category == 'test':
+            data_root = os.path.join(root_path, category)
         else:
             print("Data category wrong, only train val and test are available.")
-
         self.data_img_paths = [os.path.join(data_root, img) for img in os.listdir(data_root)]
 
-
-        if category =='train' or category == 'val':
-            self.data_labels = [1 if 'dog' == data_path.split('/')[-1].split('.')[0].split('_')[0] else 0 for data_path in self.data_img_paths]
-
-        # Test images must be sorted
         if category == 'test':
+            # Test images must be sorted
             self.data_img_paths = sorted(self.data_img_paths, key=lambda x:int(x.split('.')[-2].split('/')[-1]))
+            self.data_labels = [-1 for data_path in self.data_img_paths] 
+        else:
+            self.data_labels = [1 if 'dog' == data_path.split('/')[-1].split('.')[0].split('_')[0] else 0 for data_path in self.data_img_paths]
 
         if transforms is None:
             normalize = Transforms.Normalize(mean=[0.485, 0.456, 0.406],
                     std=[0.229, 0.224, 0.225])
-
             self.transforms = {
                 'train':
                 Transforms.Compose([
@@ -62,10 +54,7 @@ class DoCaSet(Dataset):
 
     def __getitem__(self, index):
         img_path = self.data_img_paths[index]
-        if self.category == 'test':
-            label = -1
-        else:
-            label = self.data_labels[index]
+        label = self.data_labels[index]
 
         data = Image.open(img_path)
         data = self.transforms[self.category](data)
@@ -84,7 +73,7 @@ if __name__ == '__main__':
     test_set = DoCaSet('../cat_dog', 'test')
     print(len(test_set))
 
-    train_data = val_set.__getitem__(random.randint(1,2000))
+    train_data = test_set.__getitem__(random.randint(1,2000))
     print(train_data[1])
     print(train_data[0][0])
     plt.imshow(train_data[0].T)
